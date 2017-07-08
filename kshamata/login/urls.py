906c2aededb1c088425fe191fc79f9c1c4944912
ESHAN GULHATI <eshan.gulhati@gmail.com>
2017-07-08 20:48:24 +0530

    ADD

--- kshamata/login/urls.py
+++ kshamata/login/urls.py
@@ -4,5 +4,6 @@ from . import views
 # We are adding a URL called /home
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^make/$', views.survey, name='survey')
+    url(r'^make/$', views.survey, name='survey'),
+    url(r'^addt/$', views.make, name='addtrain'),
 ]
